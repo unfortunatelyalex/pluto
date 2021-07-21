1abdba9e72ccbf38fbd41f47ecc9b51be19c7933
Alex <rezeaxtxt@gmail.com>
2021-07-21 21:09:24 +0200

    Starting off

---
+++ bot.py
@@ -0,0 +1,25 @@
+import os
+import discord
+from discord import message
+from discord.ext import commands, tasks
+from discord.utils import get
+from dotenv import load_dotenv
+import asyncio
+
+
+load_dotenv()
+
+intents = discord.Intents.default()
+intents.members = True
+
+client = commands.Bot(command_prefix='$')
+
+@client.event
+async def on_ready():
+    print('------')
+    print('Logged in as ')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+client.run(os.getenv("TOKEN"))
