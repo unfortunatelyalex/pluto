038adc139c98eeecb193e2a6c7ad435a58ad6458
Alex <rezeaxtxt@gmail.com>
2021-07-23 03:53:42 +0200

    Added activity status, clear command, embed message and a problem I have to work on (line 57-67)

--- bot.py
+++ bot.py
@@ -1,10 +1,9 @@
 import os
 import discord
-from discord import message
-from discord.ext import commands, tasks
+import random
+from discord.ext import commands
 from discord.utils import get
 from dotenv import load_dotenv
-import asyncio
 
 
 load_dotenv()
@@ -12,21 +11,81 @@ load_dotenv()
 intents = discord.Intents.default()
 intents.members = True
 
-client = commands.Bot(command_prefix='#')
+emoji = '🤡'
 
-@client.event
+
+
+# Prefix
+
+bot = commands.Bot(command_prefix='+')
+
+
+
+# Ready up message and activity status
+
+@bot.event
 async def on_ready():
     print('------')
-    print('Logged in as ')
-    print(client.user.name)
-    print(client.user.id)
+    print(f"Logged in as {bot.user} ")
+    print(f"User-ID = {bot.user.id}")
+    print(discord.__version__)
     print('------')
+    await bot.change_presence(activity=discord.Activity(type=5, name="a massive gangbang")) # Displays 'Competing in a massive gangbang'
 
 
-@client.command()
+
+# Test command
+
+@bot.command()
 async def hello(ctx):
     await ctx.send('hello')
 
 
 
-client.run(os.getenv("TOKEN"))
+@bot.command()
+async def em(ctx):
+    embed=discord.Embed(title="Test embed message", description="testing my embed skills :)", color=random.randint(0, 0xffffff))
+    embed.set_author(name=f"{ctx.message.author}", url="https://static.thenounproject.com/png/2420170-200.png", icon_url="https://static.thenounproject.com/png/2420170-200.png")
+    embed.set_thumbnail(url="https://static.thenounproject.com/png/2420170-200.png")
+    embed.add_field(name="FELD", value="WERT", inline=False)
+    embed.add_field(name="FELD 2", value="WERT 2", inline=False)
+    embed.set_footer(text="made with 💛 by alex.")
+    await ctx.send(embed=embed)
+
+
+
+# REACTION ON EVERY MESSAGE FROM GIVEN USERID
+# IF NOT IN COMMENTS, OTHER COMMANDS WONT WORK :((((((
+# I NEED HELP WITH THIS
+
+# @bot.event
+# async def on_message(message):
+#     if message.author.id == 406456604825747466:
+#         await message.add_reaction(emoji)
+    
+#     if not message.author.id == 406456604825747466:
+#         return
+
+
+
+@bot.command()
+@commands.has_permissions(manage_messages=True)
+async def clear(ctx, amount=4):
+    await ctx.channel.purge(limit=amount+1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+bot.run(os.getenv("TOKEN"))
