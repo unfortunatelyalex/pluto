c98c166fb7c1035eaec08e2e965e75acc8f696b7
Alex <rezeaxtxt@gmail.com>
2021-07-30 03:54:38 +0200

    Added error messages
    Error messages to commands that need arguments in order to run.

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import random
 import json
 import datetime
 from discord.ext import commands
-from discord.ext.commands.core import is_owner
+from discord.ext.commands import MissingRequiredArgument
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -17,7 +17,7 @@ def get_prefix(client, message):
     return prefixes[str(message.guild.id)]
 
 # Getting Prefix
-bot = commands.Bot(command_prefix=get_prefix, owner_id = 399668151475765258, case_insensitive=True)
+bot = commands.Bot(command_prefix=get_prefix, owner_id=399668151475765258, case_insensitive=True)
 
 intents = discord.Intents().all()
 intents.members = True
@@ -40,26 +40,25 @@ async def on_ready():
     print(discord.__version__)
     print('------')
     await bot.change_presence(activity=discord.Activity(type=5, name="a massive gangbang")) # Displays 'Competing in a massive gangbang'
-                                                                                  # type 0 = playing
-                                                                                  # type 1 = streaming
-                                                                                  # type 2 = listening to
-                                                                                  # type 3 = watching
-                                                                                  # type 4 = Custom (NOT SUPPORTED)
-                                                                                  # type 5 = competeting in
+                                       # playing      = type 0, name="NAME" 
+                                       # streaming    = type 1, name="NAME", url=YOUTUBE/TWITCH
+                                       # listening to = type 2, name="NAME" 
+                                       # watching     = type 3, name="NAME" 
+                                       # CUSTOM       = type 4 (NOT SUPPORTED) 
+                                       # COMPETING IN = type 5, name="NAME" 
     logs = bot.get_channel(791670764143247420)
     onstart = discord.Embed(
-        title = f"{bot.user.name} is now online",
-        color = random.randint(0, 0xffffff),
-        timestamp = datetime.datetime.now(datetime.timezone.utc)
+        title=f"{bot.user.name} is now online",
+        color=random.randint(0, 0xffffff),
+        timestamp=datetime.datetime.now(datetime.timezone.utc)
     )
     onstart.set_author(
-        name = bot.user,
-        icon_url = f"{embed_footer_icon}",
+        name=bot.user,
+        icon_url=f"{embed_footer_icon}",
     )
-    onstart.set_footer(text =f"{embed_footer}",
-    icon_url=f"{embed_footer_icon}"
-    )
-    await logs.send(embed = onstart)
+    onstart.set_footer(text=f"{embed_footer}", icon_url=f"{embed_footer_icon}")
+    await logs.send(embed=onstart)
+
 
 
 
@@ -74,10 +73,6 @@ async def on_guild_join(guild):
 
     with open('prefixes.json', 'w') as f:
         json.dump(prefixes, f, indent=4)
-
-
-
-
 
 # Prefix remove when bot leaves server
 @bot.event
@@ -95,7 +90,7 @@ async def on_guild_remove(guild):
 
 
 # Test command
-@bot.command(aliases=['hi'], description='Test if the bot is able to chat', help = "Says hello. What a nice bot.")
+@bot.command(aliases=['hi'], description='Test if the bot is able to chat', help="Says hello. What a nice bot.")
 async def hello(ctx):
     await ctx.send('hello :)')
 
@@ -104,9 +99,9 @@ async def hello(ctx):
 
 
 # Changeprefix command
-@bot.command(aliases=['setprefix'], description="Usage: .prefix [NEW PREFIX]", help = "Changes the prefix. If empty, resets the prefix to default.")
-async def prefix(ctx, prefixset = None):
-    if (not ctx.author.guild_permissions.manage_channels):
+@bot.command(aliases=['setprefix'], description="Usage: .prefix [NEW PREFIX]", help="Changes the prefix. If empty, resets the prefix to default.")
+async def prefix(ctx, prefixset=None):
+    if (not ctx.author.guild_permissions.administrator):
         await ctx.send(f'{missing_perms}')
         return
 
@@ -128,12 +123,11 @@ async def prefix(ctx, prefixset = None):
 
 
 # Token command
-
-@bot.command(aliases=['key','api', 'apikey'], description='Usage: .token', help="Shows the Token of this Bot")
+@bot.command(aliases=['key', 'api', 'apikey'], description='Usage: .token', help="Shows the Token of this Bot")
 async def token(message):
     if message.author.id != 399668151475765258:
         await message.channel.send(f'{missing_perms}')
-    
+
     if message.author.id == 399668151475765258:
         await message.channel.send('You got jebaited')
 
@@ -156,40 +150,47 @@ async def on_message(message):
 
 
 
-# Simple clear message command
-@bot.command(aliases = ['claer'], description="Usage: .clear [Amount of messages you want to delete]", help="Clears messages")
-async def clear(ctx, amount=1):
+# CLEAR MESSAGES COMMAND
+@bot.command(aliases=['claer'], description="Usage: .clear [Amount of messages you want to delete]", help="Clears messages")
+async def clear(ctx, amount : int):
     if (not ctx.author.guild_permissions.manage_messages):
         await ctx.send(f'{missing_perms}')
         return
-    await ctx.channel.purge(limit=amount+1)
+    await ctx.channel.purge(limit=amount + 1)
+@clear.error
+async def clear_error(ctx, error):
+    if isinstance(error, MissingRequiredArgument):
+        await ctx.send('Please specify an amount of messages to delete')
 
 
 
 
 
+
+# AVATAR COMMAND
 @bot.command(aliases=['av, avatar, avatar, avtar, avatr'], description="Usage: .avatar [USER @]", help="Displays a users avatar")
-async def avatar(ctx, member : discord.Member = None):
+async def avatar(ctx, member: discord.Member = None):
     if member is None:
         member = ctx.author
 
     memberAvatar = member.avatar_url
 
-    avatarEmbed = discord.Embed(title = f"{member.name}'s Avatar")
-    avatarEmbed.set_image(url = memberAvatar)
+    avatarEmbed = discord.Embed(title=f"{member.name}'s Avatar")
+    avatarEmbed.set_image(url=memberAvatar)
 
-    await ctx.send(embed = avatarEmbed)
-
-
+    await ctx.send(embed=avatarEmbed)
 
 
 
+
+
+# COMMANDS COMMAND
 @bot.command(name="commands", description="Usage: .commands", help="Lists every command available")
 async def commands(ctx):
     commands = "```"
     for command in bot.commands:
-        commands+=f"{command}\n"
-    commands+="```"
+        commands += f"{command}\n"
+    commands += "```"
     await ctx.send(commands)
 
 
@@ -203,7 +204,7 @@ async def online(ctx):
         await ctx.send(f'{not_owner}')
         await ctx.message.add_reaction(emoji)
         return
-    await bot.change_presence(status=discord.Status.online, activity=discord.Activity(type=5, name="a massive gangbang"))
+    await bot.change_presence(status=discord.Status.online,activity=discord.Activity(type=5, name="a massive gangbang"))
     await ctx.message.add_reaction('🟢')
 
 # **IDLE**
@@ -230,28 +231,95 @@ async def dnd(ctx):
 
 
 
-@bot.command(pass_context=True, name = "restart", aliases = ["r"], description="Usage: .restart", help = "Restarts the bot.")
+# PLAYING
+@bot.command(description="Usage: .playing [GAME]", help="Changes the bots activity status to 'playing ...'")
+async def playing(ctx, *, message):
+    if ctx.author.id != 399668151475765258:
+        await ctx.send(f'{not_owner}')
+        await ctx.message.add_reaction(emoji)
+        return
+    await bot.change_presence(activity=discord.Activity(type=0, name=message))
+    await ctx.message.add_reaction('👌')
+@playing.error
+async def playing_error(ctx, error):
+    if isinstance(error, MissingRequiredArgument):
+        await ctx.send('Please specify what game I should play.')
+
+
+# LISTENING TO
+@bot.command(description="Usage: .listening [VIDEO]", help="Changes the bots activity status to 'listening to ...'")
+async def listening(ctx, *, message):
+    if ctx.author.id != 399668151475765258:
+        await ctx.send(f'{not_owner}')
+        await ctx.message.add_reaction(emoji)
+        return
+    await bot.change_presence(activity=discord.Activity(type=2, name=message))
+    await ctx.message.add_reaction('👌')
+@listening.error
+async def listening_error(ctx, error):
+    if isinstance(error, MissingRequiredArgument):
+        await ctx.send('Please specify what song I should listen to.')
+
+
+# WATCHING
+@bot.command(description="Usage: .watching [VIDEO]", help="Changes the bots activity status to 'watching ...'")
+async def watching(ctx, *, message):
+    if ctx.author.id != 399668151475765258:
+        await ctx.send(f'{not_owner}')
+        await ctx.message.add_reaction(emoji)
+        return
+    await bot.change_presence(activity=discord.Activity(type=3, name=message))
+    await ctx.message.add_reaction('👌')
+@watching.error
+async def watching_error(ctx, error):
+    if isinstance(error, MissingRequiredArgument):
+        await ctx.send('Please specify what video I should watch.')
+
+
+# COMPETING IN
+@bot.command(description="Usage: .competing [VIDEO]", help="Changes the bots activity status to 'competing in ...'")
+async def compete(ctx, *, message):
+    if ctx.author.id != 399668151475765258:
+        await ctx.send(f'{not_owner}')
+        await ctx.message.add_reaction(emoji)
+        return
+    await bot.change_presence(activity=discord.Activity(type=5, name=message))
+    await ctx.message.add_reaction('👌')
+@compete.error
+async def compete_error(ctx, error):
+    if isinstance(error, MissingRequiredArgument):
+        await ctx.send('Please specify the activity in which I should compete')
+
+
+
+
+
+
+
+
+@bot.command(pass_context=True, name="restart", aliases=["r", "reset"], description="Usage: .restart", help="Restarts the bot.")
 async def restart(ctx):
     logs = bot.get_channel(791670764143247420)
     restartembed = discord.Embed(
-        title = f"{bot.user.name} is now restarting...",
-        color = random.randint(0, 0xffffff),
-        timestamp = datetime.datetime.now(datetime.timezone.utc)
+        title=f"{bot.user.name} is now restarting...",
+        color=random.randint(0, 0xffffff),
+        timestamp=datetime.datetime.now(datetime.timezone.utc)
     )
     restartembed.set_author(
-        name = ctx.author.name,
-        icon_url = ctx.author.avatar_url,
+        name=ctx.author.name,
+        icon_url=ctx.author.avatar_url,
     )
-    restartembed.set_footer(text =f"{embed_footer}",
-    icon_url=f"{embed_footer_icon}"
+    restartembed.set_footer(
+        text=f"{embed_footer}",
+        icon_url=f"{embed_footer_icon}"
     )
     if ctx.author.id != 399668151475765258:
         await ctx.send(f'{not_owner}')
         await ctx.message.add_reaction(emoji)
         return
 
-    await logs.send(embed = restartembed)
-    
+    await logs.send(embed=restartembed)
+
     await ctx.message.add_reaction("✅")
     await bot.close()
 
